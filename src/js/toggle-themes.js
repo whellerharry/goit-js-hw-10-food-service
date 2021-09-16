d58a827e1e3d4e0ad713de9230e84de8fbeb37f8
whellerharry <whellerharry7@gmail.com>
2021-09-17 00:50:31 +0300

    add theme changer

--- src/js/toggle-themes.js
+++ src/js/toggle-themes.js
@@ -11,5 +11,27 @@ switchThemeRef.addEventListener('change', onSwitchTheme);
 
 function onSwitchTheme() {
     changeSelectorBodyRef();
-
 }
+
+function changeSelectorBodyRef() {
+    if (document.body.classList.contains(Theme.LIGHT)) {
+        document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+        stillThemeOnLocalStorage(Theme.DARK);
+        return;
+    }
+        document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+        stillThemeOnLocalStorage(Theme.LIGHT);
+}
+
+function stillThemeOnLocalStorage(theme) {
+    localStorage.setItem('theme', theme)
+}
+
+function stillThemeAfterF5() {
+    const selectedTheme = localStorage.getItem('theme');
+
+    if (selectedTheme === Theme.DARK) {
+        document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+        switchThemeRef.checked = true;
+    }
+};
